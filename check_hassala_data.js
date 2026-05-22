abb0f2f0b0a2aa4a96c195f26523eeb998fdf7d4
Mo7amedTa7a <mt4658740@gmail.com>
2026-05-23 02:19:24 +0300

    chore: remove unused testing and temp files

--- check_hassala_data.js
+++
@@ -1,6 +0,0 @@
-const db = require('./backend/db');
-const rows = db.prepare('SELECT * FROM hassala').all();
-console.log(JSON.stringify(rows, null, 2));
-const balance = db.prepare('SELECT SUM(amount_in) as total_in, SUM(amount_out) as total_out FROM hassala').get();
-console.log('Balance:', balance);
-process.exit(0);
